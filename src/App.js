28323c9c9a26e60ddea87e0e3beed17f0093d460
이다혜 <dh199606@naver.com>
2025-05-01 19:35:13 +0900

    [2025/05/01]summary 길이 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"; // Router는 import하지 않음
 import Home from "./routes/Home";
 import Detail from "./routes/Details";
 
 function App() {
-  return <Router>
-    <Routes>
-      <Route path="/hello" element={<h1>Hello</h1>}></Route>
-      <Route path="/movie/:id" element={<Detail />}></Route>
-      <Route path="/" element={<Home />}></Route>
+  return (
+    <Routes> {/* HashRouter로 감싸져 있으므로 Router는 필요 없음 */}
+      <Route path="/hello" element={<h1>Hello</h1>} />
+      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
     </Routes>
-  </Router>;
+  );
 }
 
 export default App;
